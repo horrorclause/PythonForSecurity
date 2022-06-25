793e7b7ce2c815aab5e348b0bf40438e4a1b8bc7
Mario J <mjere003@gmail.com>
2022-06-25 10:29:01 -0400

    Converting strings inputs to integers #3 and #4 of Lab 1

--- PY2Class.py
+++ PY2Class.py
@@ -1,4 +1,11 @@
-x_string = input("Enter 1st number:")
-y_string = input("Enter 2nd number:")
+x_string = input("Enter 1st number:")  # Input first number as string
+y_string = input("Enter 2nd number:")  # Input second number as string
 
-print(x_string+y_string)
+print("Concatenated: ", x_string+y_string)  # Concatenate both strings
+
+
+# Converting string inputs to integers
+x = int(x_string)
+y = int(y_string)
+
+print("Sum: ", x+y)
