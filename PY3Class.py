b4d3bf83029a5c188b72ee274f9ed9d71be621b1
Mario J <mjere003@gmail.com>
2022-06-27 18:11:00 -0400

    Added Lab #4

--- PY3Class.py
+++ PY3Class.py
@@ -48,5 +48,27 @@ while counter < 10:
         print(f"Check {counter}")
 
 
+############
+# Lab 4
+############
+
+servicePorts = {}
+
+while True:
+    service = input("Please enter a service's name or type '0' to stop: ")
+    if service == "0":
+        print("Stopping...")
+        break
+
+    port = input("Please enter a port number or type '0' to stop: ")
+    if port == "0":
+        break
+
+    servicePorts[service] = port
+
+print(servicePorts)
 
 
+############
+# Lab 5
+############
