a2efaf71d770696a79635f4cd14d89a9c816dea0
Mario J <mjere003@gmail.com>
2022-06-29 17:42:36 -0400

    Added new comment

---
+++ PY4_Class/PY4_lab_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Lab2: Error Handling
+"""
+
+product = 1
+
+for i in range(4):
+    try:
+        num = int(input("Enter a number: "))
+        product *= num  # Increments up by the result of product * num
+    except:
+        print("")
+
+
+#This is a new comment
+
