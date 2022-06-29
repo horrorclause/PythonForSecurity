6d16a70fba954065786f65694e27411423ce3ce6
Mario J <mjere003@gmail.com>
2022-06-29 18:29:44 -0400

    finished Lab 2

--- PY4_Class/PY4_lab_2.py
+++ PY4_Class/PY4_lab_2.py
@@ -6,12 +6,14 @@ Lab2: Error Handling
 
 product = 1
 
-for i in range(4):
+for i in range(4):  # Will iterate 4 times
     try:
         num = int(input("Enter a number: "))
         product *= num  # Increments up by the result of product * num
     except:
-        print("")
+        print("The input is not a valid number.")
 
+print(f"The final product is {product}! ")  # Using string formatting we do not need to convert the
+                                            # variable to a string.
 
 
