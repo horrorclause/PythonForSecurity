a48e0b089e0ea665aeffea1d3c3d2148ff3b8aa2
Mario J <mjere003@gmail.com>
2022-06-29 17:27:49 -0400

    Added comments

--- PY4_Class/PY4_lab_1.py
+++ PY4_Class/PY4_lab_1.py
@@ -9,7 +9,7 @@ try:
     num2 = 0
     div = num1/num2
     print(div)
-except ZeroDivisionError:
+except ZeroDivisionError:   # Use except blocks to catch errors for graceful error handling
     print("Can't calculate it.")
-except ValueError:
+except ValueError:  # Catches the exception raised when using str instead of int in "num1"
     print("Something went wrong!")
