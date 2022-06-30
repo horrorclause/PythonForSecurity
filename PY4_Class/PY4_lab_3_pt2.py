30b299582e57dfb4bb9d1b4e1875ee1d4d3d48d2
Mario J <mjere003@gmail.com>
2022-06-29 21:18:16 -0400

    Updated lab 3

--- PY4_Class/PY4_lab_3_pt2.py
+++ PY4_Class/PY4_lab_3_pt2.py
@@ -4,12 +4,19 @@ Lab3 Pt.2: Handling Files
 Part 2 of 2 files. This file opens the "test" file
 """
 
-file = open("file.txt", "a")
+try:
 
-while True:
-    message = input("Enter Text! ('Exit' to exit): ")
-    if message.lower() == "exit":
-        print("Exiting")
-        break
-    else:
-        file.write(message + "\n")
+    file = open("file.txt", "a")
+
+    while True:
+        message = input("Enter Text! ('Exit' to exit): ")
+        if message.lower() == "exit":
+            print("Exiting")
+            break
+        else:
+            file.write(message + "\n")
+    file.close()
+
+except:
+    print("An error occurred while trying to open the file.")
+
