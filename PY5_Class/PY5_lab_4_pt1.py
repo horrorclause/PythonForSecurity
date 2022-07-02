1ee403bfe7cb9d20c74fd15cf6205bca9e732811
Mario J <mjere003@gmail.com>
2022-07-02 13:19:01 -0400

    Added Lab4 for PY5

---
+++ PY5_Class/PY5_lab_4_pt1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Py5 Lab 4 pt 1
+"""
+
+
+def main():
+    print("Hello from: ", __name__)
+
---
+++ PY5_Class/PY5_lab_4_pt2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Py5 Lab 4 pt 1
+"""
+
+import PY5_lab_4_pt1
+
+
+def main():
+    PY5_lab_4_pt1.main()
+
+
+if __name__ == '__main__':
+    print(__name__)
+    main()
