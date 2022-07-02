45822d4cc2c75a26f2d3ba85954a74429eb35c6f
Mario J <mjere003@gmail.com>
2022-07-02 14:14:05 -0400

    Added Py5 lab 8

--- PY5_Class/PY5_lab_8.py
+++ PY5_Class/PY5_lab_8.py
@@ -4,3 +4,17 @@ Py5 Lab 8
 Car Creation
 """
 
+
+class Car:
+    def __init__(self, color, windows_number, price):
+        self.color = color
+        self.windows_number = windows_number
+        self.price = price
+
+
+car1 = Car("red", 4, 10000)
+car2 = Car('Black', 6, 25000)
+
+#print(vars(car1)) prints out all attributes
+print(car2.price)
+print(car1.windows_number)
