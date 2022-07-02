21742a999727438c9a64b13205c73c67360e08fd
Mario J <mjere003@gmail.com>
2022-07-02 10:21:31 -0400

    finished py5 lab 2

--- PY5_Class/PY5_lab_2.py
+++ PY5_Class/PY5_lab_2.py
@@ -9,7 +9,7 @@ def returned_list(first, second):
 
 
 def returned_dict(first, second):
-    return {first, second}
+    return {first: second}
 
 
 def returned_tuple(first, second):
@@ -27,5 +27,15 @@ def main():
     dict_res = returned_dict(first_val, second_val)
     tup_res = returned_tuple(first_val, second_val)
     none_res = returned_none(first_val, second_val)
+
+    print(f"{list_res} and its type is: {type(list_res)}")
+    print(f"{dict_res} and its type is: {type(dict_res)}")
+    print(f"{tup_res} and its type is: {type(tup_res)}")
+    print(f"{none_res} and its type is: {type(none_res)}")
+
+
+main()
+
+
 
 
